bdf479b5f8dc40177b5414a5e3559a3df06115c2
Axel Hagelberg Strid <axel.hagelbergstridh@gmail.com>
2024-10-31 12:39:42 +0100

    Addition of break in while loop in askToLit

--- Adventure_Game/FirePlace.java
+++ Adventure_Game/FirePlace.java
@@ -43,7 +43,7 @@ public class FirePlace {
                                 break;
                         
                         }
-                        
+                        break;
                     } 
                 }
 
